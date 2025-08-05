90a107552668d2127a576df6706736bcfad46124
tonystark2306 <dat.ntt210177@sis.hust.edu.vn>
2025-08-05 11:02:55 +0700

    commit

---
+++ EasyPosterApp/CameraVC.swift
@@ -0,0 +1,178 @@
+//
+//  CameraVC.swift
+//  EasyPosterApp
+//
+//  Created by iKame Elite Fresher 2025 on 8/4/25.
+//
+
+import UIKit
+import AVFoundation
+
+protocol CameraVCDelegate: AnyObject {
+    func didCaptureImage(_ image: UIImage)
+}
+
+class CameraVC: UIViewController, AVCapturePhotoCaptureDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var flashButton: UIButton!
+    @IBOutlet weak var switchCamButton: UIButton!
+    @IBOutlet weak var galleryButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var captureButton: UIButton!
+    @IBOutlet weak var previewView: UIView!
+    
+    private let captureSession = AVCaptureSession()
+    private var outputPhoto = AVCapturePhotoOutput()
+    private var previewLayer: AVCaptureVideoPreviewLayer!
+    private var captureDevice: AVCaptureDevice?
+    private var flashOn = 0
+
+    weak var delegate: CameraVCDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        checkPermission()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer?.frame = previewView.bounds
+    }
+    
+    private func checkPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            setupCamera()
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    DispatchQueue.main.async {
+                        self.setupCamera()
+                    }
+                }
+            }
+        default:
+            showAlert()
+        }
+    }
+
+    private func setupCamera() {
+        captureSession.beginConfiguration()
+        captureSession.sessionPreset = .photo
+        
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+            let input = try? AVCaptureDeviceInput(device: device),
+            captureSession.canAddInput(input) {
+            captureSession.addInput(input)
+                self.captureDevice = device
+            }
+        if captureSession.canAddOutput(outputPhoto) {
+            captureSession.addOutput(outputPhoto)
+        }
+        
+        captureSession.commitConfiguration()
+        setupPreview()
+        DispatchQueue.global().async {
+            self.captureSession.startRunning()
+        }
+    }
+    
+    private func showAlert() {
+        let alert = UIAlertController(title: "Cần quyền truy cập Camera", message: "Vui lòng cấp quyền truy cập Camera trong cài đặt", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+    
+    private func setupPreview() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = previewView.bounds
+        previewView.layer.insertSublayer(previewLayer, at: 0)
+    }
+    
+    @IBAction func didTappedFlash(_ sender: Any) {
+        flashOn += 1
+        var imageName = ""
+        if (flashOn % 3 == 1) {
+            imageName = "icFlashOn"
+        }
+        else if (flashOn % 3 == 2) {
+            imageName = "icFlashAuto"
+        }
+        else {
+            imageName = "icFlashOff"
+        }
+        flashButton.setImage(UIImage(named: imageName), for: .normal)
+    }
+    
+    @IBAction func didTappedClose(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func didTappedCapture(_ sender: Any) {
+        let settings = AVCapturePhotoSettings()
+        if (flashOn % 3 == 1) {
+            settings.flashMode = .on
+        }
+        else if (flashOn % 3 == 2) {
+            settings.flashMode = .auto
+        }
+        else {
+            settings.flashMode = .off
+        }
+        outputPhoto.capturePhoto(with: settings, delegate: self)
+    }
+    
+    @IBAction func didTappedGallery(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        picker.allowsEditing = false
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func didTappedSwitch(_ sender: Any) {
+        guard let currentInput = captureSession.inputs.first as? AVCaptureDeviceInput else { return }
+        
+        captureSession.beginConfiguration()
+        captureSession.removeInput(currentInput)
+        
+        let position: AVCaptureDevice.Position = currentInput.device.position == .back ? .front : .back
+        
+        if let newDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: position),
+           let nextInput = try? AVCaptureDeviceInput(device: newDevice) {
+            captureSession.addInput(nextInput)
+            self.captureDevice = newDevice
+        }
+        captureSession.commitConfiguration()
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard error == nil else {
+            print("Lỗi: \(error!)")
+            return
+        }
+        
+        guard let imageData = photo.fileDataRepresentation(),
+              let image = UIImage(data: imageData) else {
+            print("Could not convert photo to image")
+            return
+        }
+        delegate?.didCaptureImage(image)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        if let image = info[.originalImage] as? UIImage {
+            delegate?.didCaptureImage(image)
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+}
--- EasyPosterApp/ViewController.swift
+++ EasyPosterApp/ViewController.swift
@@ -1,12 +1,6 @@
-//
-//  ViewController.swift
-//  EasyPosterApp
-//
-//  Created by Trần Đạt on 26/7/25.
-//
-
 import UIKit
 import PhotosUI
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet var originnalImageView: [UIImageView]!
@@ -17,16 +11,20 @@ class ViewController: UIViewController {
         didSet {
             if !listImage.isEmpty {
                 originnalImageView.forEach({
-                    $0.image = listImage[$0.tag]
+                    if $0.tag < listImage.count {
+                        $0.image = listImage[$0.tag]
+                    }
                 })
             }
         }
     }
+    
     private var listText: [NSAttributedString] {
         return nameTextFields.map({
             .init(string: $0.text ?? "Khuyết danh", attributes: textAttributes)
         })
     }
+    
     private var finalImage: UIImage?
     
     lazy var textAttributes: [NSAttributedString.Key: Any] = {
@@ -44,12 +42,55 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapUpload(_ sender: UIButton) {
-        var config = PHPickerConfiguration()
-        config.selectionLimit = 3
-        config.filter = .images
-        let picker = PHPickerViewController.init(configuration: config)
-        picker.delegate = self
-        present(picker, animated: true)
+        let alert = UIAlertController(title: "Select Options", message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Take Photos", style: .default, handler: { _ in
+            self.requestCameraPermissionAndOpenCamera()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Choose from Library", style: .default, handler: { _ in
+            var config = PHPickerConfiguration()
+            config.selectionLimit = 5
+            config.filter = .images
+            let picker = PHPickerViewController(configuration: config)
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(alert, animated: true)
+    }
+    
+    private func requestCameraPermissionAndOpenCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            openCamera()
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                DispatchQueue.main.async {
+                    granted ? self.openCamera() : self.showPermissionAlert()
+                }
+            }
+        default:
+            showPermissionAlert()
+        }
+    }
+
+    private func openCamera() {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("Thiết bị không có camera")
+            return
+        }
+        let cameraPicker = CameraVC()
+        cameraPicker.delegate = self
+        navigationController?.pushViewController(cameraPicker, animated: true)
+    }
+
+    private func showPermissionAlert() {
+        let alert = UIAlertController(title: "Không có quyền truy cập camera", message: "Vui lòng cấp quyền trong Cài đặt.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
     }
     
     @IBAction func tapCreatePoster(_ sender: UIButton) {
@@ -58,48 +99,78 @@ class ViewController: UIViewController {
         let width = frame.width
         let height = frame.height - textBoundHeight
         let renderer = UIGraphicsImageRenderer(bounds: frame)
+        
+        guard listImage.count == 5 else {
+            print("Cần đủ 5 ảnh để tạo poster.")
+            return
+        }
+        
         let image = renderer.image { context in
-            let frame1 = CGRect(x: 0, y: 0, width: width * 7/20, height: height)
-            let firstImage = listImage.first!
-            firstImage.draw(in: frame1)
+            let frame1 = CGRect(x: 0, y: 0, width: width * 1/4, height: height)
+            listImage[0].draw(in: frame1)
             
-            let thirdImage = listImage[2]
-            let frame3 = CGRect(x: width * 13/20, y: 0, width: width * 7/20, height: height)
-            thirdImage.draw(in: frame3)
+            let frame5 = CGRect(x: width * 3/4, y: 0, width: width * 1/4, height: height)
+            listImage[4].draw(in: frame5)
             
-            // Tạo path hình thang đều theo đì zai
             let rectPath = UIBezierPath()
-            rectPath.move(to: .init(x: width/4, y: .zero))
-            rectPath.addLine(to: .init(x: width * 3/4, y: .zero))
-            rectPath.addLine(to: .init(x: width * 13/20, y: height))
-            rectPath.addLine(to: .init(x: width * 7/20, y: height))
+            rectPath.move(to: CGPoint(x: width/7, y: 0))
+            rectPath.addLine(to: CGPoint(x: width * 3/7, y: 0))
+            rectPath.addLine(to: CGPoint(x: width * 3/8, y: height))
+            rectPath.addLine(to: CGPoint(x: width * 1/4, y: height))
             rectPath.close()
-            
             rectPath.addClip()
             
-            let secondImage = listImage[1]
-            let frame2 = CGRect(x: width/4, y: 0, width: width/2, height: height)
-            secondImage.draw(in: frame2)
+            let frame2 = CGRect(x: width * 1/7, y: 0, width: width * 2/7, height: height)
+            listImage[1].draw(in: frame2)
             
-            // Bỏ clip đi để còn vẽ típ
+            UIGraphicsGetCurrentContext()?.resetClip()
+            
+            let rectPath2 = UIBezierPath()
+            rectPath2.move(to: CGPoint(x: width * 3/7, y: 0))
+            rectPath2.addLine(to: CGPoint(x: width * 4/7, y: 0))
+            rectPath2.addLine(to: CGPoint(x: width * 5/8, y: height))
+            rectPath2.addLine(to: CGPoint(x: width * 3/8, y: height))
+            rectPath2.close()
+            rectPath2.addClip()
+            
+            let frame3 = CGRect(x: width * 3/8, y: 0, width: width/4, height: height)
+            listImage[2].draw(in: frame3)
+            
+            UIGraphicsGetCurrentContext()?.resetClip()
+            
+            let rectPath3 = UIBezierPath()
+            rectPath3.move(to: CGPoint(x: width * 4/7, y: 0))
+            rectPath3.addLine(to: CGPoint(x: width * 6/7, y: 0))
+            rectPath3.addLine(to: CGPoint(x: width * 3/4, y: height))
+            rectPath3.addLine(to: CGPoint(x: width * 5/8, y: height))
+            rectPath3.close()
+            rectPath3.addClip()
+            
+            let frame4 = CGRect(x: width * 4/7, y: 0, width: width * 2/7, height: height)
+            listImage[3].draw(in: frame4)
+            
             UIGraphicsGetCurrentContext()?.resetClip()
             
             let borderPath = UIBezierPath()
-            borderPath.move(to: .init(x: width/4, y: .zero))
-            borderPath.addLine(to: .init(x: width * 7/20, y: height))
-            borderPath.move(to: .init(x: width * 3/4, y: .zero))
-            borderPath.addLine(to: .init(x: width * 13/20, y: height))
+            borderPath.move(to: CGPoint(x: width/7, y: 0))
+            borderPath.addLine(to: CGPoint(x: width/4, y: height))
+            borderPath.move(to: CGPoint(x: width * 3/7, y: 0))
+            borderPath.addLine(to: CGPoint(x: width * 3/8, y: height))
+            borderPath.move(to: CGPoint(x: width * 4/7, y: 0))
+            borderPath.addLine(to: CGPoint(x: width * 5/8, y: height))
+            borderPath.move(to: CGPoint(x: width * 6/7, y: 0))
+            borderPath.addLine(to: CGPoint(x: width * 3/4, y: height))
+            
             UIColor.white.setStroke()
             borderPath.lineWidth = 4
             borderPath.stroke()
             
             UIColor.systemBrown.setFill()
-            UIBezierPath(rect: CGRect(origin: .init(x: .zero, y: height), size: .init(width: width, height: textBoundHeight))).fill()
+            UIBezierPath(rect: CGRect(x: 0, y: height, width: width, height: textBoundHeight)).fill()
             
-            let listAttributedText = listText
-            listAttributedText[0].draw(in: .init(x: 0, y: height, width: width/3, height: textBoundHeight))
-            listAttributedText[1].draw(in: .init(x: width/3, y: height, width: width/3, height: textBoundHeight))
-            listAttributedText[2].draw(in: .init(x: width * 2/3, y: height, width: width/3, height: textBoundHeight))
+            listText[0].draw(in: CGRect(x: 0, y: height, width: width/3, height: textBoundHeight))
+            listText[1].draw(in: CGRect(x: width/3, y: height, width: width/3, height: textBoundHeight))
+            listText[2].draw(in: CGRect(x: width * 2/3, y: height, width: width/3, height: textBoundHeight))
         }
         finalImage = image
         finalImageView.image = image
@@ -112,24 +183,59 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: CameraVCDelegate {
+    func didCaptureImage(_ image: UIImage) {
+        if listImage.count < 5 {
+            listImage.append(image)
+        } else {
+            print("Đã đủ 5 ảnh")
+        }
+    }
+}
+
+// MARK: - PHPicker Delegate
+
 extension ViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         picker.dismiss(animated: true)
-        guard results.count == 3 else {
-            print("Cần chọn đủ 3 ảnh")
+        guard results.count == 5 else {
+            print("Cần chọn đủ 5 ảnh")
             return
         }
-        var listImage: [UIImage] = []
+        var loadedImages: [UIImage] = []
+        let dispatchGroup = DispatchGroup()
+        
         results.forEach { result in
+            dispatchGroup.enter()
             result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
-                guard let self else { return }
-                listImage.append(image as! UIImage)
-                if listImage.count == 3 {
-                    DispatchQueue.main.async {
-                        self.listImage = listImage
-                    }
+                if let img = image as? UIImage {
+                    loadedImages.append(img)
                 }
+                dispatchGroup.leave()
             }
         }
+        
+        dispatchGroup.notify(queue: .main) { [weak self] in
+            guard let self = self else { return }
+            self.listImage = loadedImages
+        }
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        if let image = info[.originalImage] as? UIImage {
+            if listImage.count < 5 {
+                listImage.append(image)
+            } else {
+                print("Đã đủ 5 ảnh")
+            }
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
     }
 }
